52416c7ef27826769149adcc792c4066f9e26a61
Pritam Dash <ipritamdash@gmail.com>
2025-03-17 15:06:19 +0530

    Your commit message

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,42 +1,41 @@
-import express from "express";
-import dotenv from "dotenv";
-import { connectDB } from "./library/db.js";
+import express from "express"
+import dotenv from "dotenv"
+import {connectDB} from "./library/db.js"
 import cookieParser from "cookie-parser";
 import cors from "cors";
+
 import path from "path";
-import { fileURLToPath } from "url";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-import { app, server } from "./library/socket.js";
+import { app , server } from "./library/socket.js"
 
-dotenv.config();
+dotenv.config()
 
-const PORT = process.env.PORT || 5000;
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
-app.use(express.json({ limit: "10mb" }));
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
+
+app.use(express.json({ limit: "10mb" })); 
 app.use(cookieParser());
 app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin:"http://localhost:5173" ,
     credentials: true,
-}));
+})
+);
 
-// API routes
-app.use("/api/auth", authRoutes);
-app.use("/api/messages", messageRoutes);
+app.use("/api/auth" , authRoutes);
+app.use("/api/messages" , messageRoutes);
 
-// Serve Frontend in Production
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "frontend", "dist")));
-
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
     });
-}
+  }
 
-server.listen(PORT, () => {
-    console.log(`Server is running on port: ${PORT}`);
-    connectDB();
-});
+server.listen(PORT , ()=>{
+    console.log(`server is running on port:${PORT}`);
+    connectDB()
+})  
